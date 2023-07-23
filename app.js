7a1de066436ef07b499b3d1f59235fe747e1e125
kumarabhishekbansal <kumarabhishekbansal>
2023-07-23 16:50:54 +0530

    all commit5

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ const server=app.listen(PORT,()=>{
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://chatting1.onrender.com",
+    origin: ["https://chatting1.onrender.com","http://localhost:3000"],
     credentials: true,
   },
 });
